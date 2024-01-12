6d43a950b69a33698cc9109b9480e374a4adc930
Robert Funesti <rfunesti@essextech.org>
2024-01-12 12:59:45 -0500

    CameraController

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     PlayerMovement movement;
+    PlayerCamera camera;
 
     void Awake()
     {
         movement = GetComponent<PlayerMovement>();
+        camera = GetComponentInChildren<PlayerCamera>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        camera.PitchInput = Input.GetAxisRaw("Mouse Y");
+        camera.YawInput = Input.GetAxisRaw("Mouse X");
         Vector3 forward = Camera.main.transform.forward * Input.GetAxisRaw("Vertical");
         Vector3 right = Camera.main.transform.right * Input.GetAxisRaw("Horizontal");
         movement.moveDirection = forward + right;
